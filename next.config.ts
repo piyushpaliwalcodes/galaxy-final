d0d768c6fe250bb66ebcbbf557d09c344ebd7b98
Piyush Paliwal <piyushpaliwalofficial868@gmail.com>
2025-05-08 18:10:51 +0530

    config updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  experimental: {} as any,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      "s3dev.imgix.net",
+      "www.reshot.com",
+      "cdn.prod.website-files.com",
+    ],
+  },
 };
 
 export default nextConfig;
