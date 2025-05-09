4208fbce3f238deb5d4b1235b98a9a75b7999bb1
Piyush Paliwal <piyushpaliwalofficial868@gmail.com>
2025-05-09 13:06:41 +0530

    signuppage show

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,8 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton  forceRedirectUrl={"/"}  />
-              <SignUpButton  forceRedirectUrl={"/"} />
+              <SignUpButton forceRedirectUrl={"/"} />
+              <SignInButton forceRedirectUrl={"/"} />
             </SignedOut>
             <SignedIn>
               <UserButton />
